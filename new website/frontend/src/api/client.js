32df44c7c37656f40b6dbb512204c4fd08f50176
CPE3C-cruz-romer <2023105164@ms.bulsu.edu.ph>
2026-04-21 15:42:47 +0800

    n

--- new website/frontend/src/api/client.js
+++ new website/frontend/src/api/client.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
 
-const DEFAULT_API_URL = 'https://job-aggregator-website.onrender.com/api';
+const DEFAULT_API_URL = "https://job-aggregator-website.onrender.com/api/";
 
 const rawBaseUrl =
-  process.env.REACT_APP_API_URL ||
-  process.env.REACT_APP_API_BASE_URL ||
-  DEFAULT_API_URL;
+    process.env.REACT_APP_API_URL ||
+    process.env.REACT_APP_API_BASE_URL ||
+    DEFAULT_API_URL;
 
 const normalizedBaseUrl = rawBaseUrl.replace(/\/+$/, '');
 
 const api = axios.create({
-  baseURL: normalizedBaseUrl,
+    baseURL: normalizedBaseUrl,
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('accessToken');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+    const token = localStorage.getItem('accessToken');
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
 });
 
 export default api;
